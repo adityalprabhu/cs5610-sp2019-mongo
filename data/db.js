e27fba9f1b10e696b8db1bcd66d9c36e68515bab
Aditya Prabhu <adityalprabhu@gmail.com>
2019-04-07 07:37:37 -0400

    adding heroku url

--- data/db.js
+++ data/db.js
@@ -5,6 +5,6 @@ module.exports = function () {
     var   connectionString = 'mongodb://heroku_kjcxnp96:n1td8eecph3t0vo4oo8cvqa6ep@ds133556.mlab.com:33556/heroku_kjcxnp96';
 
 
-    connectionString += databaseName;
+    // connectionString += databaseName;
     mongoose.connect(connectionString);
 };
