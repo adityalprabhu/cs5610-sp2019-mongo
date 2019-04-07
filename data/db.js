910237d2854084691789d3daa8c03039efe40aff
Aditya Prabhu <adityalprabhu@gmail.com>
2019-04-07 07:27:40 -0400

    adding heroku db

--- data/db.js
+++ data/db.js
@@ -1,7 +1,10 @@
 module.exports = function () {
     const mongoose = require('mongoose');
-    const databaseName = 'white-board';
-    var   connectionString = 'mongodb://localhost/';
+    const databaseName = 'heroku_kjcxnp96';
+    // var   connectionString = 'mongodb://localhost/';
+    var   connectionString = 'mongodb://heroku_kjcxnp96:n1td8eecph3t0vo4oo8cvqa6ep@ds133556.mlab.com:33556/heroku_kjcxnp96';
+
+
     connectionString += databaseName;
     mongoose.connect(connectionString);
 };
